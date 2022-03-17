d5b13a95e5c671440dd7b1a2f48c4232c6fbd1bd
Paulo Uilian <uilianlbarros@gmail.com>
2022-03-17 14:17:26 -0300

    Implementará interface das telas dos cards em react

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,24 @@
-export default function Card({text, icon, alt}){
+export default function Card({ text, icon, alt}) {
+    const question = 'Porque alguma coisa?'
+    const answer = 'Porque sim'
     return (
-        <div className="card">
-            <p>{text}</p>
-            <img src={icon} alt={alt} />
-        </div>
+        <>
+            <div className="card">
+                <p>{text}</p>
+                <img src={icon} alt={alt} />
+            </div>
+            <div className="card-question">
+                <p>{question}</p>
+                <button><img src="./images/spin.svg" alt="girar" /></button>
+            </div>
+            <div className="card-answer">
+                <p>{answer}</p>
+                <div className="buttons">
+                    <button className="bad">Não lembrei</button>
+                    <button className="medium">Quase não lembrei</button>
+                    <button className="good">Zap</button>
+                </div>
+            </div>
+        </>
     )
 }
