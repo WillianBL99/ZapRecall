51f8dce86d37e568b00d7f3595442225e491080d
Paulo Uilian <uilianlbarros@gmail.com>
2022-03-18 19:44:38 -0300

    Componentizará botão da tela de inicio

--- src/components/InitialScreen.jsx
+++ src/components/InitialScreen.jsx
@@ -1,27 +1,35 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 
 import ZapRecallLogo from '../assets/logo.svg'
 
 
-export default function InitialScreen(){
-    
+export default function InitialScreen() {
+
     const [visible, setVisible] = useState(true)
 
     return (
-        visible?
-        <section className="login">
-            <InitialLogoRecall />
-            <button className="btn-start" onClick={()=>setVisible(false)}>Iniciar Recall!</button>
-        </section>:
-        <></>
+        visible ?
+            <section className="login">
+                <InitialLogoRecall />
+                <Button setVisible={(value) => setVisible(value)} />
+            </section> :
+            <></>
     )
 }
 
-function InitialLogoRecall(){
+function InitialLogoRecall() {
     return (
         <div className="logo">
             <img src={ZapRecallLogo} alt="Logo" />
             <h1>ZapRecall</h1>
         </div>
+    )
+}
+
+function Button({ setVisible }) {
+    return (
+        <button className="btn-start" onClick={() => setVisible(false)}>
+            Iniciar Recall!
+        </button>
     )
 }
