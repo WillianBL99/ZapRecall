be71ca1a4e8a0115f0896c42ad650631a1536b75
Paulo Uilian <uilianlbarros@gmail.com>
2022-03-18 19:30:57 -0300

    Realizará o import de imagens para o componente Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,9 @@
+import ZapRecallLogo from '../assets/logo.svg'
+
 export default function Header() {
     return (
         <header>
-            <img src="./images/logo.svg" alt="logo" />
+            <img src={ZapRecallLogo} alt="logo" />
             <h1>ZapRecall</h1>
         </header>
     )
