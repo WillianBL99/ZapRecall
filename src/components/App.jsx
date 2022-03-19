ad2baa2b6b71d23297cbb2e0610c9b93a3c1d8fd
Paulo Uilian <uilianlbarros@gmail.com>
2022-03-19 14:19:36 -0300

    Implementará variáveis para tamanho das fonts

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,9 @@
 import Header from "./Header"
 import MainScreen from "./MainScreen"
 
+import '../style/reset.css'
+import '../style/index.css'
+
 export default function App() {
     return (
         <>
